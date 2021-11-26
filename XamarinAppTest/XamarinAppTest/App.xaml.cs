d673ae604551b3e9a408164c5c34fa6d4e1306be
Osiel Nicolás <chilparosales@gmail.com>
2021-11-25 21:48:41 -0600

    Se agregaron los primeros elementos básicos a un diseño base.

--- XamarinAppTest/XamarinAppTest/App.xaml.cs
+++ XamarinAppTest/XamarinAppTest/App.xaml.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace XamarinAppTest
 {
+    /// <summary>
+    /// La clase App representa la aplicación de Xamarin.Forms como un todo y se hereda de Xamarin.Forms.Application.
+    /// </summary>
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
+            
+            // Esta propiedad controlará la primera pantalla que ve el usuario.
             MainPage = new MainPage();
         }
 
+        #region Métodos para controlar los eventos de ciclo de vida
         protected override void OnStart()
         {
         }
@@ -24,5 +27,6 @@ namespace XamarinAppTest
         protected override void OnResume()
         {
         }
+        #endregion
     }
 }
--- XamarinAppTest/XamarinAppTest/MainPage.xaml.cs
+++ XamarinAppTest/XamarinAppTest/MainPage.xaml.cs
@@ -2,11 +2,25 @@
 
 namespace XamarinAppTest
 {
+    /// <summary>
+    /// Una página de contenido simplemente muestra su contenido.
+    /// </summary>
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            var layout = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical
+            };
+
+            layout.Children.Add(new Label { Text = "Ingrese su nombre" });
+            layout.Children.Add(new Entry());
+            layout.Children.Add(new Button { Text = "Aceptar" });
+
+            this.Content = layout;
         }
     }
 }
